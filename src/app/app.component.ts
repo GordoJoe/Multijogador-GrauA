b9229c8e60d383035cf5c5a2c931b49778d5d744
GordoJoe <leonardo2845@hotmail.com>
2018-10-05 10:25:38 -0300

    arrumado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-<<<<<<< HEAD
-  name = 'Angular AEEW';
-=======
-  name = 'Angulaaaaar';
->>>>>>> origin/master
+  name = 'Angular';
 }
